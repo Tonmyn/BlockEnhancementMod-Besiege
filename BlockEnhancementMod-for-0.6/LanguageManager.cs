0f9362e814bcb2035880d58e5f25fe6654cc30c0
X极限战士X <1014723833@qq.com>
2018-06-16 21:39:30 +0800

    常规提交

--- BlockEnhancementMod-for-0.6/LanguageManager.cs
+++ BlockEnhancementMod-for-0.6/LanguageManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using UnityEngine;
 
 namespace BlockEnhancementMod
 {
@@ -17,11 +20,27 @@ namespace BlockEnhancementMod
 
         public Language currentLanguage = Language.Chinese;
 
+        public Dictionary<int, string> languageDic;
+
+        [Obsolete]
         public Language GetLanguage()
         {
             Language L = new Language();
 
+            string configPath = Application.dataPath + "/Config.xml";
+
+            XmlDocument xDoc = new XmlDocument();
+            
+            xDoc.Load(configPath);    //加载Xml文件,全路径    
+
+            XmlNode xmlNode = xDoc.GetElementsByTagName("Language")[0];
+
+            string language = xmlNode.InnerText;
+
             return L;
         }
+
+
+
     }
 }
