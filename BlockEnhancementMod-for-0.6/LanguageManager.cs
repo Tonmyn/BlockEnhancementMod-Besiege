ce0016c22b04e55fff48852db5a5572348cb4587
X极限战士X <1014723833@qq.com>
2018-06-16 19:55:53 +0800

    添加语言管理器类

---
+++ BlockEnhancementMod-for-0.6/LanguageManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlockEnhancementMod
+{
+    class LanguageManager : SingleInstance<LanguageManager>
+    {
+        public override string Name { get; } = "Language Manager";
+
+        public enum Language
+        {
+            Chinese = 0,
+            English = 1
+        }
+
+        public Language currentLanguage = Language.Chinese;
+
+        public Language GetLanguage()
+        {
+            Language L = new Language();
+
+            return L;
+        }
+    }
+}
