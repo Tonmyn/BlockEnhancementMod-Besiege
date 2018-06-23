bc63686d346fd07e85086e22ffdec7e251d8b0de
Patrick Xu <xuxiangzhe@msn.com>
2018-06-23 20:07:54 +1000

    Rockets can now save targets, and can explode at a pre-set distance from target. Dictionary for targets is shared by rockets and camera. Fixed dictionary missing bug. Renamed some functions and variables.

--- BlockEnhancementMod-for-0.6/EnhancementBlocks/Rocket.cs
+++ BlockEnhancementMod-for-0.6/EnhancementBlocks/Rocket.cs
@@ -10,17 +10,22 @@ namespace BlockEnhancementMod.Blocks
     {
 
         MToggle GuidedRocketToggle;
+        MToggle CloseRangeExplosionToggle;
         MSlider GuidedRocketTorqueSlider;
         MSlider GuideDelaySlider;
+        MSlider CloseExploRangeSlider;
         MKey LockTargetKey;
 
         public List<KeyCode> lockKeys = new List<KeyCode> { KeyCode.Delete };
+        public int selfIndex;
 
         public bool guidedRocketIsActivated = false;
+        public bool closeRangeExploActivated = false;
         public bool hasFired = false;
         public float fireTime = 0f;
         public bool fireTimeRecorded = false;
         public float torque = 100f;
+        public float closeRange = 0f;
         public float guideDelay = 0f;
         public float previousAngleDiff = 0;
         public float angleDiffCumulative = 0;
@@ -33,10 +38,22 @@ namespace BlockEnhancementMod.Blocks
             GuidedRocketToggle = AddToggle("追踪目标", "TrackingRocket", guidedRocketIsActivated);
             GuidedRocketToggle.Toggled += (bool value) =>
             {
-                guidedRocketIsActivated = GuidedRocketTorqueSlider.DisplayInMapper = LockTargetKey.DisplayInMapper = GuideDelaySlider.DisplayInMapper = value;
+                guidedRocketIsActivated = GuidedRocketTorqueSlider.DisplayInMapper = CloseRangeExplosionToggle.DisplayInMapper = LockTargetKey.DisplayInMapper = GuideDelaySlider.DisplayInMapper = value;
                 ChangedProperties();
             };
             BlockDataLoadEvent += (XDataHolder BlockData) => { guidedRocketIsActivated = GuidedRocketToggle.IsActive; };
+
+            CloseRangeExplosionToggle = AddToggle("近炸", "CloseeRangeExplo", closeRangeExploActivated);
+            CloseRangeExplosionToggle.Toggled += (bool value) =>
+            {
+                closeRangeExploActivated = CloseExploRangeSlider.DisplayInMapper = value;
+                ChangedProperties();
+            };
+            BlockDataLoadEvent += (XDataHolder BlockData) => { closeRangeExploActivated = CloseRangeExplosionToggle.IsActive; };
+
+            CloseExploRangeSlider = AddSlider("近炸距离", "closeRange", closeRange, 0, 10, false);
+            CloseExploRangeSlider.ValueChanged += (float value) => { closeRange = value; ChangedProperties(); };
+            BlockDataLoadEvent += (XDataHolder BlockData) => { closeRange = CloseExploRangeSlider.Value; };
 
             GuidedRocketTorqueSlider = AddSlider("火箭扭转力度", "torqueOnRocket", torque, 0, 10000, false);
             GuidedRocketTorqueSlider.ValueChanged += (float value) => { torque = value; ChangedProperties(); };
@@ -47,10 +64,18 @@ namespace BlockEnhancementMod.Blocks
             BlockDataLoadEvent += (XDataHolder BlockData) => { guideDelay = GuideDelaySlider.Value; };
 
             LockTargetKey = AddKey("锁定目标", "lockTarget", lockKeys);
+            LockTargetKey.InvokeKeysChanged();
             //LockTargetKey.KeysChanged += ChangedProperties;
 
             //Add reference to TimedRocket
             rocket = gameObject.GetComponent<TimedRocket>();
+            selfIndex = GetComponent<BlockBehaviour>().BuildIndex;
+
+            //Make sure the target list is present
+            if (!Machine.Active().gameObject.GetComponent<TargetScript>())
+            {
+                Machine.Active().gameObject.AddComponent<TargetScript>();
+            }
 
 #if DEBUG
             ConsoleController.ShowMessage("火箭添加进阶属性");
@@ -62,8 +87,52 @@ namespace BlockEnhancementMod.Blocks
         {
             GuidedRocketToggle.DisplayInMapper = value;
             GuidedRocketTorqueSlider.DisplayInMapper = value && guidedRocketIsActivated;
+            CloseRangeExplosionToggle.DisplayInMapper = value && guidedRocketIsActivated;
+            CloseExploRangeSlider.DisplayInMapper = value && closeRangeExploActivated;
             GuideDelaySlider.DisplayInMapper = value && guidedRocketIsActivated;
             LockTargetKey.DisplayInMapper = value && guidedRocketIsActivated;
+        }
+
+        public override void LoadConfiguration(XDataHolder BlockData)
+        {
+            if (BlockData.HasKey("bmt-" + "RocketTarget"))
+            {
+                SaveTargetToDict(BlockData.ReadInt("bmt-" + "RocketTarget"));
+            }
+        }
+
+        public override void SaveConfiguration(XDataHolder BlockData)
+        {
+            if (Machine.Active().GetComponent<TargetScript>().previousTargetDic.ContainsKey(selfIndex))
+            {
+                BlockData.Write("bmt-" + "RocketTarget", Machine.Active().GetComponent<TargetScript>().previousTargetDic[selfIndex]);
+            }
+        }
+
+        protected override void OnSimulateStart()
+        {
+            // Trying to read previously saved target
+            int targetIndex = -1;
+            BlockBehaviour targetBlock = new BlockBehaviour();
+            // Read the target's buildIndex from the dictionary
+            try
+            {
+                Machine.Active().GetComponent<TargetScript>().previousTargetDic.TryGetValue(selfIndex, out targetIndex);
+            }
+            catch (Exception)
+            {
+                ConsoleController.ShowMessage("Cannot get target index");
+            }
+            // Aquire target block's transform from the target's index
+            try
+            {
+                Machine.Active().GetBlockFromIndex(targetIndex, out targetBlock);
+                target = Machine.Active().GetSimBlock(targetBlock).transform;
+            }
+            catch (Exception)
+            {
+                ConsoleController.ShowMessage("Cannot get target block's transform");
+            }
         }
 
         protected override void OnSimulateFixedUpdate()
@@ -74,8 +143,23 @@ namespace BlockEnhancementMod.Blocks
                 ConsoleController.ShowMessage("Ray casted");
                 if (Physics.Raycast(ray, out RaycastHit hit))
                 {
-                    target = hit.collider.transform;
-                    //fireTime = Time.time;
+                    target = hit.transform;
+
+                    // Trying to save target's buildIndex to the dictionary
+                    // If not a machine block, set targetIndex to -1
+                    int targetIndex = -1;
+                    try
+                    {
+                        targetIndex = target.GetComponent<BlockBehaviour>().BuildIndex;
+                    }
+                    catch (Exception)
+                    {
+                        ConsoleController.ShowMessage("Not a machine block");
+                    }
+                    if (targetIndex != -1)
+                    {
+                        SaveTargetToDict(target.GetComponent<BlockBehaviour>().BuildIndex);
+                    }
                 }
             }
         }
@@ -97,6 +181,11 @@ namespace BlockEnhancementMod.Blocks
                         // Calculating the rotating axis
                         Vector3 velocityNormarlized = GetComponent<Rigidbody>().velocity.normalized;
                         Vector3 positionDiff = target.position - transform.position;
+                        if (closeRangeExploActivated && positionDiff.magnitude <= closeRange)
+                        {
+                            rocket.OnExplode();
+                            return;
+                        }
                         float angleDiff = Vector3.Angle(positionDiff.normalized, velocityNormarlized);
                         Vector3 rotatingAxis = -Vector3.Cross(positionDiff.normalized, velocityNormarlized);
                         float angularSpeed = (angleDiff - previousAngleDiff) / Time.fixedDeltaTime;
@@ -131,6 +220,21 @@ namespace BlockEnhancementMod.Blocks
             if (rocket.hasFired && collision.impulse.magnitude > 1)
             {
                 rocket.OnExplode();
+            }
+        }
+
+        private void SaveTargetToDict(int BlockID)
+        {
+            // Make sure the dupicated key exception is handled
+            try
+            {
+                Machine.Active().GetComponent<TargetScript>().previousTargetDic.Add(selfIndex, BlockID);
+            }
+            catch (Exception)
+            {
+                // Remove the old record, then add the new record
+                Machine.Active().GetComponent<TargetScript>().previousTargetDic.Remove(selfIndex);
+                Machine.Active().GetComponent<TargetScript>().previousTargetDic.Add(selfIndex, BlockID);
             }
         }
     }
