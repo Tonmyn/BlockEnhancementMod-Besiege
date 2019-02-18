2c0a2110155de181671c82b3f560c5c15a5d42af
X极限战士X <1014723833@qq.com>
2019-02-18 22:02:24 +0800

    禁用打组事件功能

--- BlockEnhancementMod/Mod.cs
+++ BlockEnhancementMod/Mod.cs
@@ -15,7 +15,7 @@ namespace BlockEnhancementMod
             mod = new GameObject("Block Enhancement Mod");
             Object.DontDestroyOnLoad(mod);
             mod.AddComponent<EnhancementBlockController>();
-            mod.AddComponent<Zone>();
+            //mod.AddComponent<Zone>();
 
             //Controller.Instance.transform.SetParent(mod.transform);
             ModSettingUI.Instance.transform.SetParent(mod.transform);
@@ -23,8 +23,8 @@ namespace BlockEnhancementMod
             MessageController.Instance.transform.SetParent(mod.transform);
             RocketsController.Instance.transform.SetParent(mod.transform);
 
-            EnhancementEventsController events = mod.AddComponent<EnhancementEventsController>(); ;
-            ModEvents.RegisterCallback(1, events.OnGroup);
+            //EnhancementEventsController events = mod.AddComponent<EnhancementEventsController>(); ;
+            //ModEvents.RegisterCallback(1, events.OnGroup);
         }
     }
 
