9b93d61320be69dee832b0ce6635ec549b4227b9
X极限战士X <1014723833@qq.com>
2019-06-02 23:22:10 +0800

    完成雷达脚本的 圆台形状触发区 的搭建

--- BlockEnhancementMod/EnhancementBlock/Blocks/Rocket.cs
+++ BlockEnhancementMod/EnhancementBlock/Blocks/Rocket.cs
@@ -76,6 +76,8 @@ namespace BlockEnhancementMod.Blocks
         public bool activeGuide = true;
         public bool targetAquired = false;
         public bool searchStarted = false;
+        public GameObject Radar;
+
 
         //Cluster value multiplier
         private readonly float bombValue = 64;
@@ -347,7 +349,16 @@ namespace BlockEnhancementMod.Blocks
                 {
                     bombExplosiveCharge = Mathf.Clamp(explosiveCharge, 0f, 1.5f);
                 }
-            }
+
+                Radar = new GameObject("Radar");
+                Radar.transform.SetParent(transform);
+                Radar.transform.position = transform.position;
+                Radar.transform.rotation = transform.rotation;
+                Radar.transform.localPosition = Vector3.forward * 0.5f;
+
+                var rs = Radar.AddComponent<RadarScript>();
+                rs.searchAngle = searchAngle;
+            }         
         }
 
         public override void SimulateUpdateAlways_EnhancementEnable()
@@ -1381,4 +1392,132 @@ namespace BlockEnhancementMod.Blocks
         }
     }
 
+    class RadarScript : MonoBehaviour
+    {
+        public float radius = 1000f;
+        public float safetyRadius = 30f;
+        public float searchAngle = 20f;
+
+        public bool Switch { get; set; } = false;
+        public searchMode SearchMode { get; set; } = searchMode.Auto;
+
+        public Target target;
+
+
+
+        public event VoidDel OnTarget;
+
+        public enum searchMode
+        {
+            Auto = 0,
+            Manual = 1
+        }
+
+        void Awake()
+        {
+            OnTarget += () => { };
+        }
+
+        void Start()
+        {
+            createCone(searchAngle,safetyRadius,radius);
+#if DEBUG
+            var mr = gameObject.AddComponent<MeshRenderer>();
+            mr.material.color = Color.green;
+#endif
+
+            void createCone(float angle,float topRadius,float bottomRadius)
+            {            
+                float topHeight = topRadius;
+                float height = bottomRadius - topHeight;
+
+                float radiusTop = Mathf.Tan(angle * 0.5f * Mathf.Deg2Rad) * topHeight;
+                float radiusBottom = Mathf.Tan(angle * 0.5f * Mathf.Deg2Rad) * bottomRadius;
+
+                //越高越精细
+                int numVertices = 20;
+
+                Vector3 myTopCenter = Vector3.up * topHeight;
+                Vector3 myBottomCenter = myTopCenter + Vector3.up * height;
+                //构建顶点数组和UV数组
+                Vector3[] vertices = new Vector3[numVertices * 2 + 2];
+                Vector2[] uvs = new Vector2[vertices.Length];
+                //顶圆中心点放第一个，底圆中心点放最后一个
+                vertices[0] = myTopCenter;
+                vertices[vertices.Length - 1] = myBottomCenter;
+                
+                for (int i = 0; i < numVertices; i++)
+                {
+                    float angleStep = 2 * Mathf.PI * i / numVertices;
+                    float angleSin = Mathf.Sin(angleStep);
+                    float angleCos = Mathf.Cos(angleStep);
+
+                    vertices[i + 1] = new Vector3(radiusTop * angleCos, myTopCenter.magnitude, radiusTop * angleSin);
+                    vertices[i + 1 + numVertices] = new Vector3(radiusBottom * angleCos, myBottomCenter.magnitude, radiusBottom * angleSin);
+
+                    uvs[i] = new Vector2(1.0f * i / numVertices, 1);
+                    uvs[i + numVertices] = new Vector2(1.0f * i / numVertices, 0);
+                }
+
+                int[] tris = new int[numVertices * 6 + numVertices * 6];
+                int index = 0;
+                //画下圆面
+                for (int i = 0; i < numVertices; i++)
+                {
+                    tris[index++] = 0;
+                    tris[index++] = i + 1;
+                    tris[index++] = (i == numVertices - 1) ? 1 : i + 2;
+                }
+                //画斜面
+                for (int i = 0; i < numVertices; i++)
+                {
+                    int ip1 = i + 2;
+                    if (ip1 > numVertices)
+                        ip1 = 1;
+
+                    tris[index++] = i + 1;
+                    tris[index++] = i + 1 + numVertices;
+                    tris[index++] = ip1 + numVertices;
+
+                    tris[index++] = i + 1;
+                    tris[index++] = ip1 + numVertices;
+                    tris[index++] = ip1;
+                }
+                //画上圆面
+                for (int i = 0; i < numVertices; i++)
+                {
+                    tris[index++] = 2 * numVertices + 1;
+                    tris[index++] = (i == numVertices - 1) ? numVertices + 1 : numVertices + i + 2;
+                    tris[index++] = numVertices + i + 1;
+                }
+
+                var mf = gameObject.AddComponent<MeshFilter>();
+                mf.mesh.vertices = vertices;
+                mf.mesh.triangles = tris;
+                mf.mesh.uv = uvs;
+                mf.mesh.RecalculateBounds();
+                mf.mesh.RecalculateNormals();
+
+                var mc = gameObject.AddComponent<MeshCollider>();
+                mc.sharedMesh = GetComponent<MeshFilter>().mesh;
+                mc.convex = true;
+                mc.isTrigger = true;
+
+            }
+
+        }
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("into");
+
+            OnTarget.Invoke();
+        }
+
+    }
+
+
+    class Target
+    {
+
+    }
 }
