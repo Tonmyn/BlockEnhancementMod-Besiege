b5614d9b452e922af78f582846fddf1444399d47
X极限战士X <1014723833@qq.com>
2020-02-18 22:59:00 +0800

    添加语言字段

--- BlockEnhancementMod/EnhancementBlock/EnhancementBlock.cs
+++ BlockEnhancementMod/EnhancementBlock/EnhancementBlock.cs
@@ -284,9 +284,9 @@ namespace BlockEnhancementMod
         public override void SafeAwake()
         {
             base.SafeAwake();
-            AddSpeedKey = /*BB.*/AddKey("加速", "Add Speed", KeyCode.Equals);
-            ReduceSpeedKey = /*BB.*/AddKey("减速", "Reduce Speed", KeyCode.Minus);
-            ChangeSpeedValue = /*BB.*/AddValue("更改速度", "Change Speed", 0.1f);
+            AddSpeedKey = /*BB.*/AddKey(LanguageManager.Instance.CurrentLanguage.AddSpeed,"Add Speed", KeyCode.Equals);
+            ReduceSpeedKey = /*BB.*/AddKey( LanguageManager.Instance.CurrentLanguage.ReduceSpeed, "Reduce Speed",KeyCode.Minus);
+            ChangeSpeedValue = /*BB.*/AddValue(LanguageManager.Instance.CurrentLanguage.ChangeSpeed, "Change Speed", 0.1f);
         }
 
         //public override void DisplayInMapper(bool value)
