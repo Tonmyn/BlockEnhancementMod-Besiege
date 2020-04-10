0d4dd113d2c74fbc58b967d8449aa3d215966ece
Patrick Xu <px.patrick@gmail.com>
2020-04-10 23:04:52 +1000

    Progress save: fix a bool

--- BlockEnhancementMod/EnhancementBlock/Blocks/Rocket.cs
+++ BlockEnhancementMod/EnhancementBlock/Blocks/Rocket.cs
@@ -312,8 +312,8 @@ namespace BlockEnhancementMod
                         {
                             if (radar.RadarType == RadarScript.RadarTypes.ActiveRadar)
                             {
+                                radar.meshRenderer.enabled = radar.canBeOverridden && GuidedRocketShowRadarToggle.IsActive;
                                 radar.meshCollider.enabled = radar.canBeOverridden;
-                                radar.meshRenderer.enabled = radar.canBeOverridden;
                                 radar.canBeOverridden = !radar.canBeOverridden;
                                 if (!radar.canBeOverridden)
                                 {
