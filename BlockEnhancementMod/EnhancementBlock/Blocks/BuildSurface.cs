51ff9ce5d244724ed3e17f1f80a68a719db6559a
Tomee <1014723833@qq.com>
2023-03-15 21:25:45 +0800

    添加蒙皮块自定义皮肤透明功能

--- BlockEnhancementMod/EnhancementBlock/Blocks/BuildSurface.cs
+++ BlockEnhancementMod/EnhancementBlock/Blocks/BuildSurface.cs
@@ -11,19 +11,27 @@ namespace BlockEnhancementMod
     {
 
         MToggle /*colliderToggle,*/ kinematicToggle;
+        MToggle transparentSupportToggle;
         //MSlider redSlider, greenSlider, blueSlider;
-        MSlider alphaSlider,radiusSlider;
+        MSlider /*alphaSlider,*/radiusSlider;
         BuildSurface buildSurface;
         [SerializeField]
         MeshRenderer materialRenderer;
         //SurfaceFragmentController fragmentController;
-
+        [SerializeField]
+        private Material transparentMaterial;
+        private Material baseMaterial;
         [SerializeField]
         private bool isWood = false;
         [SerializeField]
         private bool isDefaultSkin = false;
+        [SerializeField]
+        private int visualValue = 0;
         private int lastSkinValue = 0;
         private Shader oldShader;
+        [SerializeField]
+        private bool transparentUsed = false;
+
         public override void SafeAwake()
         {
             base.SafeAwake();
@@ -31,6 +39,8 @@ namespace BlockEnhancementMod
 
             //colliderToggle = AddToggle("Collider", "collider", false);
             kinematicToggle = AddToggle("Kinematic", "kinematic", false);
+            transparentSupportToggle = AddToggle("Transparent Support", "Transparent", false);
+            transparentSupportToggle.Toggled += transparentChanged;
 
             //redSlider = AddSlider("red", "red", 1f, 0f, 1f);
             //redSlider.ValueChanged += colorChanged;
@@ -38,30 +48,33 @@ namespace BlockEnhancementMod
             //greenSlider.ValueChanged += colorChanged;
             //blueSlider = AddSlider("blue", "blue", 1f, 0f, 1f);
             //blueSlider.ValueChanged += colorChanged;
-            alphaSlider = AddSlider("Alpha", "Alpha", 1f, 0f, 1f);
-            alphaSlider.ValueChanged += colorChanged;
+            //alphaSlider = AddSlider("Alpha", "Alpha", 1f, 0f, 1f);
+            //alphaSlider.ValueChanged += colorChanged;
 
             radiusSlider = AddSlider("Radius", "Radius", 1f, 0.1f, 1f);
             radiusSlider.ValueChanged += radiusChanged;
 
 
             //buildSurface.material.ValueChanged += materialChanged;
-            buildSurface.material.ValueChanged += (value) => { DisplayInMapper(EnhancementEnabled); };
-            buildSurface.hue.ValueChanged += (color) => { colorChanged(0f); };
-            buildSurface.paint.Toggled += (value) => { DisplayInMapper(EnhancementEnabled); };
+            //buildSurface.material.ValueChanged += (value) => { DisplayInMapper(EnhancementEnabled); };
+            //buildSurface.hue.ValueChanged += (color) => { colorChanged(0f); };
+            //buildSurface.paint.Toggled += (value) => { DisplayInMapper(EnhancementEnabled); };
+
 
             //materialChanged(buildSurface.material.Value);
 
-            //StartCoroutine(wait());
+            StartCoroutine(wait());
 
-            //IEnumerator wait()
-            //{
-            //    yield return new WaitUntil(() => BB.Visual != null);
-            //    buildSurface.hue.ValueChanged += (color) => { materialRenderer.material.color = color; };
-            //    BB.Visual.ValueChanged += skinChanged;
-            //    skinChanged(BB.Visual.Value);
-            //    yield break;
-            //}
+            IEnumerator wait()
+            {
+                yield return new WaitUntil(() => BB.Visual != null);
+                //buildSurface.hue.ValueChanged += (color) => { materialRenderer.material.color = color; };
+                buildSurface.Visual.ValueChanged += skinChanged;
+                materialRenderer = transform.FindChild("Vis").GetComponent<MeshRenderer>();
+                //BB.Visual.ValueChanged += skinChanged;
+                //skinChanged(BB.Visual.Value);
+                yield break;
+            }
 #if DEBUG
             ConsoleController.ShowMessage("蒙皮块添加进阶属性");
 #endif
@@ -75,19 +88,39 @@ namespace BlockEnhancementMod
         //        Debug.Log("reset");
         //    }
         //}
+
+        private void judgeDispaly_transparentToggle()
+        {
+            var factor = ((int)Mathf.Clamp01(visualValue) == 0 ? false : true);
+            transparentSupportToggle.DisplayInMapper = EnhancementEnabled && factor && buildSurface.Visual != null;
+        }
+
         public override void DisplayInMapper(bool enhance)
         {
             /*colliderToggle.DisplayInMapper =*/ kinematicToggle.DisplayInMapper = radiusSlider.DisplayInMapper = enhance;
             ///*redSlider.DisplayInMapper = greenSlider.DisplayInMapper = blueSlider.DisplayInMapper =*/ alphaSlider.DisplayInMapper = enhance && isWood /*&& !isDefaultSkin*/;
             ///
+            judgeDispaly_transparentToggle();
+            //Debug.Log("dim" + buildSurface.paint.IsActive);
+            //alphaSlider.DisplayInMapper = enhance && buildSurface.paint.DisplayInMapper && buildSurface.paint.IsActive;
+            //if (alphaSlider.DisplayInMapper)
+            //{
+            //    materialRenderer = transform.FindChild("Vis").GetComponent<MeshRenderer>();
+            //    oldShader = materialRenderer.material.shader;
+            //}
+        }
 
-            Debug.Log("dim" + buildSurface.paint.IsActive);
-            alphaSlider.DisplayInMapper = enhance && buildSurface.paint.DisplayInMapper && buildSurface.paint.IsActive;
-            if (alphaSlider.DisplayInMapper)
+        public override void BuildingUpdateAlways_EnhancementEnabled()
+        {
+            base.BuildingUpdateAlways_EnhancementEnabled();
+
+            if (lastSkinValue != visualValue)
             {
-                materialRenderer = transform.FindChild("Vis").GetComponent<MeshRenderer>();
-                oldShader = materialRenderer.material.shader;
+                judgeDispaly_transparentToggle();
+
+                lastSkinValue = visualValue;
             }
+
         }
 
         public override void OnSimulateStart_EnhancementEnabled()
@@ -157,6 +190,14 @@ namespace BlockEnhancementMod
                     yield break;
                 }
             }
+
+            var value = transparentSupportToggle.IsActive;
+            Debug.Log("trans: " + value.ToString());
+            if (value)
+            {
+                transparentChanged(value);
+                //transparentUsed = true;
+            }
         }
         //public override void OnSimulateStartAlways()
         //{
@@ -178,22 +219,22 @@ namespace BlockEnhancementMod
         //}
         private void colorChanged(float value)
         {
+            Debug.Log("??");
+            //if (materialRenderer == null || EnhancementEnabled == false) return;
 
-            if (materialRenderer == null || EnhancementEnabled == false) return;
-
-            if (alphaSlider.Value >= 0.99f)
-            {
-                materialRenderer.material.shader = oldShader;
-            }
-            else
-            {
-                materialRenderer.material.shader = Shader.Find("Transparent/Diffuse");
-            }
-            //var color = new Color(redSlider.Value, greenSlider.Value, blueSlider.Value, alphaSlider.Value * 6f);
-            //var color = materialRenderer.material.color;
-            var color = buildSurface.hue.Value;
-            color = new Color(color.r, color.g, color.b, alphaSlider.Value * 6f);
-            materialRenderer.material.color = color;
+            //if (alphaSlider.Value >= 0.99f)
+            //{
+            //    materialRenderer.material.shader = oldShader;
+            //}
+            //else
+            //{
+            //    materialRenderer.material.shader = Shader.Find("Transparent/Diffuse");
+            //}
+            ////var color = new Color(redSlider.Value, greenSlider.Value, blueSlider.Value, alphaSlider.Value * 6f);
+            ////var color = materialRenderer.material.color;
+            //var color = buildSurface.hue.Value;
+            //color = new Color(color.r, color.g, color.b, alphaSlider.Value * 6f);
+            //materialRenderer.material.color = color;
         }
         private void radiusChanged(float value)
         {
@@ -206,6 +247,27 @@ namespace BlockEnhancementMod
             {
                 point.transform.localScale = Vector3.one * value;
             }
+        }
+
+        private void transparentChanged(bool value)
+        {
+            Debug.Log(buildSurface.Visual + "|" + visualValue);
+            if (OptionsMaster.skinsEnabled == false || visualValue == 0) return;
+
+            Debug.Log("trans changed: " + value.ToString());
+
+            if (value)
+            {
+                //oldShader = materialRenderer.material.shader;
+                //materialRenderer.material.shader = Shader.Find("Transparent/Diffuse");
+                materialRenderer.material = transparentMaterial;
+            }
+            else
+            {
+                //materialRenderer.material.shader = oldShader;
+                materialRenderer.material = baseMaterial;
+            }
+
         }
         //private void skinChanged(int value)
         //{
@@ -224,6 +286,42 @@ namespace BlockEnhancementMod
         //        yield break;
         //    }
         //}
+
+        private void skinChanged(int value)
+        {
+            if (!EnhancementEnabled) return;
+
+            Debug.Log("skin changed" + value.ToString());
+
+            visualValue = value;
+            Debug.Log(lastSkinValue + "||" + visualValue);
+
+            if (visualValue != lastSkinValue)
+            {
+                transparentSupportToggle.IsActive = false;
+            }
+
+            if (visualValue != 0)
+            {
+                if (visualValue != lastSkinValue)
+                {
+                    Destroy(baseMaterial);
+                    Destroy(transparentMaterial);
+
+                    baseMaterial = new Material(materialRenderer.material);
+                    transparentMaterial = intiMaterial(materialRenderer.material);
+                }
+            }
+
+            Material intiMaterial(Material material)
+            {
+                var _material = new Material(material);
+                _material.name = "Transparent Material";
+                _material.shader = Shader.Find("Transparent/Diffuse");
+
+                return _material;
+            }
+        }
 
         //private void refreshColorSlider(bool display)
         //{
